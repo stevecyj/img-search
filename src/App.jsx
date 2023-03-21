ff93f224c63d58ded3512a9b08fe13b2e4ea4386
Steve <ycsteven.tsao@gmail.com>
2023-03-22 01:14:20 +0800

    handle loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,18 @@
-import { useState, useEffect } from 'react';
-import './App.css';
 import { Title } from '@/components/Title.jsx';
 import { Form } from '@/components/Form.jsx';
 import { GridResults } from '@/components/GridResults.jsx';
 import { useFormQuery } from '@/hook/index.js';
 
 function App() {
-  const { query, handleSubmit } = useFormQuery();
+  const { query, handleSubmit, isLoading, handleLoading } = useFormQuery();
 
   return (
     <div>
       <Title />
-      <Form handleSubmit={handleSubmit} />
-      {query.length > 0 && <GridResults query={query} />}
+      <Form handleSubmit={handleSubmit} isLoading={isLoading} />
+      {query.length > 0 && (
+        <GridResults query={query} handleLoading={handleLoading} />
+      )}
     </div>
   );
 }
--- src/components/GridResults.jsx
+++ src/components/GridResults.jsx
@@ -1,12 +1,17 @@
-import { Card } from '@/components/Card.jsx';
+import { useEffect } from 'react';
 import PropTypes from 'prop-types';
+import { Card } from '@/components/Card.jsx';
 import { useQuery } from '@tanstack/react-query';
 import { getImages } from '@/utils/index.js';
+import { Loading } from '@/components/Loading.jsx';
 
-const GridResults = ({ query }) => {
+const GridResults = ({ query, handleLoading }) => {
   const { data, isLoading, error, isError } = useQuery(['images', query], () =>
     getImages(query),
   );
+
+  useEffect(() => handleLoading(isLoading), [isLoading]);
+
   if (isLoading) return <Loading />;
 
   if (isError) return <p>{error.message}</p>;
@@ -29,5 +34,6 @@ const GridResults = ({ query }) => {
 
 GridResults.propTypes = {
   query: PropTypes.string,
+  handleLoading: PropTypes.func,
 };
 export { GridResults };
