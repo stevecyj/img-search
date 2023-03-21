9692d3db910b2c8579649964bdba2281b7ae5db9
Steve <ycsteven.tsao@gmail.com>
2023-03-21 11:07:58 +0800

    feat: query string to state query

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,7 +1,21 @@
+import {useState} from "react";
+
 export const Form = () => {
+  const [query,setQuery] = useState('');
+  const handleSubmit = e => {
+    e.preventDefault()
+    const target = e.target;
+    const {form} = Object.fromEntries(new FormData(target));
+    console.log(form);
+    if (form.toString().trim().length === 0) return
+    setQuery(form.toString())
+    target.reset()
+    target.focus()
+  };
+
   return (
-    <form>
-      <input type="text" placeholder="Example: superman"/>
+    <form onSubmit={handleSubmit}>
+      <input type="text" placeholder="Example: batman" name="form"/>
       <button>Search</button>
     </form>
   );
